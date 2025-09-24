0e4d8fbe6be620eb88f6ee2162967f65ab965ace
Evst404 <al.evst96@gmail.com>
2025-09-24 18:07:40 +0200

    Update settings.py

--- project_prime_flower_shop/settings.py
+++ project_prime_flower_shop/settings.py
@@ -74,10 +74,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#EMAIL_HOST = 'smtp.mail.ru'
-#EMAIL_PORT = 587
-#EMAIL_USE_TLS = True
-#EMAIL_HOST_USER = 'your@email.ru' 
-#EMAIL_HOST_PASSWORD = 'your_password'  
+ 
